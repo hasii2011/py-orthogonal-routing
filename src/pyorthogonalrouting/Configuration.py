4086ab08f7d49b55f53acbccdd2bf08a7e1d0cf7
Humberto Sanchez II <n3875yffbt@privaterelay.appleid.com>
2024-05-10 12:55:30 -0500

    <Enhancement>[Configuration]: <Two more options>
    [
    * Added new options
    * Made .ini file options readable]
    
    [https://github.com/hasii2011/py-orthogonal-routing/issues/2]

--- src/pyorthogonalrouting/Configuration.py
+++ src/pyorthogonalrouting/Configuration.py
@@ -14,15 +14,19 @@ from codeallybasic.SingletonV3 import SingletonV3
 
 from pyorthogonalrouting.Rect import Rect
 
-DEFAULT_SHAPE_MARGIN:         str  = '20'
-DEFAULT_GLOBAL_BOUNDS_MARGIN: str  = '50'
-DEFAULT_GLOBAL_BOUNDS:        Rect = Rect(left=0, top=0, width=500, height=500)
+DEFAULT_SHAPE_MARGIN:              str  = '20'
+DEFAULT_GLOBAL_BOUNDS_MARGIN:      str  = '50'
+DEFAULT_SOURCE_EDGE_DISTANCE:      str  = '0.5'
+DEFAULT_DESTINATION_EDGE_DISTANCE: str  = '0.5'
+DEFAULT_GLOBAL_BOUNDS:             Rect = Rect(left=0, top=0, width=500, height=500)
 
 SECTION_MAIN: Section = Section(
     [
-        ConfigurationNameValue(name=PropertyName('shapeMargin'),        defaultValue=DEFAULT_SHAPE_MARGIN),
-        ConfigurationNameValue(name=PropertyName('globalBoundsMargin'), defaultValue=DEFAULT_GLOBAL_BOUNDS_MARGIN),
-        ConfigurationNameValue(name=PropertyName('globalBounds'),       defaultValue=DEFAULT_GLOBAL_BOUNDS.__str__()),
+        ConfigurationNameValue(name=PropertyName('shapeMargin'),             defaultValue=DEFAULT_SHAPE_MARGIN),
+        ConfigurationNameValue(name=PropertyName('globalBoundsMargin'),      defaultValue=DEFAULT_GLOBAL_BOUNDS_MARGIN),
+        ConfigurationNameValue(name=PropertyName('globalBounds'),            defaultValue=DEFAULT_GLOBAL_BOUNDS.__str__()),
+        ConfigurationNameValue(name=PropertyName('sourceEdgeDistance'),      defaultValue=DEFAULT_SOURCE_EDGE_DISTANCE),
+        ConfigurationNameValue(name=PropertyName('destinationEdgeDistance'), defaultValue=DEFAULT_DESTINATION_EDGE_DISTANCE),
     ]
 )
 
@@ -40,7 +44,19 @@ class Configuration(ConfigurationProperties, metaclass=SingletonV3):
     def __init__(self):
         super().__init__(baseFileName='pyorthogonalrouting.ini', moduleName='pyorthogonalrouting', sections=PY_ORTHOGONAL_ROUTING_SECTIONS)
 
+        self._configParser.optionxform = self._toStr    # type: ignore
         self._loadConfiguration()
+
+    def _toStr(self, optionString: str) -> str:
+        """
+        Override base method
+
+        Args:
+            optionString:
+
+        Returns: The option string unchanged
+        """
+        return optionString
 
     @property
     @configurationGetter(sectionName=MAIN_SECTION_NAME, deserializeFunction=int)
@@ -70,4 +86,24 @@ class Configuration(ConfigurationProperties, metaclass=SingletonV3):
     @globalBounds.setter
     @configurationSetter(sectionName=MAIN_SECTION_NAME)
     def globalBounds(self, newValue: Rect):
+        pass
+
+    @property
+    @configurationGetter(sectionName=MAIN_SECTION_NAME, deserializeFunction=float)
+    def sourceEdgeDistance(self) -> float:
+        return 0.0
+
+    @sourceEdgeDistance.setter
+    @configurationSetter(sectionName=MAIN_SECTION_NAME)
+    def sourceEdgeDistance(self, newValue: float):
+        pass
+
+    @property
+    @configurationGetter(sectionName=MAIN_SECTION_NAME, deserializeFunction=float)
+    def destinationEdgeDistance(self) -> float:
+        return 0.0
+
+    @destinationEdgeDistance.setter
+    @configurationSetter(sectionName=MAIN_SECTION_NAME)
+    def destinationEdgeDistance(self, newValue: float):
         pass
